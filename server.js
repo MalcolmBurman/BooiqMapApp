82c16fc914d4d96660e94acce3aa981b9d2a001d
malcolmburman <malcolmburman@hotmail.com>
2025-05-09 09:29:14 +0200

    :d

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
 app.all("/api/auth/{*any}", toNodeHandler(auth));
+app.use(express.json());
 
 app.get("/getProperties", async (req, res) => {
   try {
